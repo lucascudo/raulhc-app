73f396993976306f34c1e18f27850f5bfe4168e6
Cascudo <lucas.sisau@gmail.com>
2015-10-05 19:46:19 -0300

    Inserindo datepicker na agenda

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,34 @@
-angular.module('starter.controllers', ['starter.services'])
-
+angular.module('starter.controllers', [
+    'ionic-datepicker',
+    'starter.services'
+])
 .controller('AgendaCtrl', function($ionicLoading, $scope, RaulFactory) {
-    $ionicLoading.show({ template: 'loading' });
-    RaulFactory.getAgenda({}, function (agenda) {
-        $scope.agenda = agenda.eventos;
-        $ionicLoading.hide();
-    });
+    var loadAgenda = function (date) {
+        date = date.getUTCFullYear() + '-' + ('00' + (date.getUTCMonth() + 1)).slice(-2) + '-' + ('00' + date.getUTCDate()).slice(-2);
+        $ionicLoading.show({ template: 'loading' });
+        RaulFactory.getAgenda({ "date": date }, function (agenda) {
+            $scope.agenda = agenda.eventos;
+            $ionicLoading.hide();
+        });
+    };
+    
+    $scope.datepickerObject = {
+      titleLabel: 'Selecione uma data',  //Optional
+      todayLabel: 'Hoje',  //Optional
+      closeLabel: 'Fechar',  //Optional
+      setLabel: 'Aplicar',  //Optional
+      inputDate: new Date(),    //Optional
+      weekDaysList: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],   //Optional
+      monthList: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+      from: new Date(),   //Optional
+      callback: function (date) {    //Mandatory
+        if (typeof date !== "undefined") {
+            loadAgenda(date);
+        }
+      }
+    };
+    
+    loadAgenda(new Date());
 })
 .controller('TemGenteCtrl', function($ionicLoading, $scope, RaulFactory) {
     /*
--- www/js/services.js
+++ www/js/services.js
@@ -2,15 +2,16 @@ angular.module('starter.services', [ 'ngResource' ])
 
 .factory('RaulFactory', function($resource) {
     var baseUrl = "http://raulhc.cc/";
-    return $resource("RaulHC WebService", { }, {
+    return $resource("RaulHC WebService", {}, {
 	getAgenda: {
-            url: baseUrl + "Agenda/JSON",
-			method: "GET",
+            url: baseUrl + "/Agenda/JSON?data=:date",
+            params: { date: "@date" },
+            method: "GET",
             headers: {
                 "Content-Type": "application/json; charset=UTF-8",
                 "Accept-Charset": "UTF-8"
             }
-		},
+        },
         getTemGente: {
             url: baseUrl + "tem-gente?type=json",
 			method: "GET",
