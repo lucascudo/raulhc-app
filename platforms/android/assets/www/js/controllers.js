09e3bd09cfc540f426b149476454ebce5706349a
Lucas Teixeira Rocha <lucas.sisau@gmail.com>
2015-10-05 22:30:04 +0000

    Inserindo splash e recompilando

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,11 +1,34 @@
-angular.module('starter.controllers', ['starter.services'])
-
+angular.module('starter.controllers', [
+    'ionic-datepicker',
+    'starter.services'
+])
 .controller('AgendaCtrl', function($ionicLoading, $scope, RaulFactory) {
-    $ionicLoading.show({ template: 'loading' });
-    RaulFactory.getAgenda({}, function (agenda) {
-        $scope.agenda = agenda.eventos;
-        $ionicLoading.hide();
-    });
+    var loadAgenda = function (date) {
+        date = date.getUTCFullYear() + '-' + ('00' + (date.getUTCMonth() + 1)).slice(-2) + '-' + ('00' + date.getUTCDate()).slice(-2);
+        $ionicLoading.show({ template: 'loading' });
+        RaulFactory.getAgenda({ "date": date }, function (agenda) {
+            $scope.agenda = agenda.eventos;
+            $ionicLoading.hide();
+        });
+    };
+    
+    $scope.datepickerObject = {
+      titleLabel: 'Selecione uma data',
+      todayLabel: 'Hoje',
+      closeLabel: 'Fechar',
+      setLabel: 'Aplicar',
+      inputDate: new Date(),
+      weekDaysList: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+      monthList: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+      from: new Date(),
+      callback: function (date) {
+        if (typeof date !== "undefined") {
+            loadAgenda(date);
+        }
+      }
+    };
+    
+    loadAgenda(new Date());
 })
 .controller('TemGenteCtrl', function($ionicLoading, $scope, RaulFactory) {
     /*
--- platforms/android/assets/www/js/services.js
+++ platforms/android/assets/www/js/services.js
@@ -1,23 +1,24 @@
 angular.module('starter.services', [ 'ngResource' ])
 
 .factory('RaulFactory', function($resource) {
-	var baseUrl = "http://raulhc.cc/";
-    return $resource("RaulHC WebService", { }, {
-		getAgenda: {
-            url: baseUrl + "Agenda/JSON",
-			method: "GET",
+    var baseUrl = "http://raulhc.cc/";
+    return $resource("RaulHC WebService", {}, {
+	getAgenda: {
+            url: baseUrl + "/Agenda/JSON?data=:date",
+            params: { date: "@date" },
+            method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                //"charset": "UTF-8"
+                "Content-Type": "application/json; charset=UTF-8",
+                "Accept-Charset": "UTF-8"
             }
-		},
+        },
         getTemGente: {
             url: baseUrl + "tem-gente?type=json",
 			method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                //"charset": "UTF-8"
+                "Content-Type": "application/json; charset=UTF-8",
+                "Accept-Charset": "UTF-8"
             }
-		}
-	});
+        }
+    });
 });
