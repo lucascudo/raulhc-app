4c81a2c90a638be80d18143f97a70572d800c689
Cascudo <lucas.sisau@gmail.com>
2015-10-05 18:57:21 -0300

    Corrigindo service do raulhc

--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,23 @@
 angular.module('starter.services', [ 'ngResource' ])
 
 .factory('RaulFactory', function($resource) {
-	var baseUrl = "http://raulhc.cc/";
+    var baseUrl = "http://raulhc.cc/";
     return $resource("RaulHC WebService", { }, {
 	getAgenda: {
             url: baseUrl + "Agenda/JSON",
 			method: "GET",
             headers: {
-                "Content-Type": "application/json; charset=UTF-8"
+                "Content-Type": "application/json; charset=UTF-8",
+                "Accept-Charset": "UTF-8"
             }
 		},
         getTemGente: {
             url: baseUrl + "tem-gente?type=json",
 			method: "GET",
             headers: {
-                "Content-Type": "application/json; charset=UTF-8"
+                "Content-Type": "application/json; charset=UTF-8",
+                "Accept-Charset": "UTF-8"
             }
-		}
-	});
+        }
+    });
 });
