93f3c0f6228166bde0ccdb2065fe004aaa1bc2c0
Cascudo <lucas.sisau@gmail.com>
2015-12-08 18:23:56 -0300

    Inserindo material theme

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', [
   'ionic',
+  'ionic-material',
   'starter.controllers',
   'starter.directives',
   'starter.services'
